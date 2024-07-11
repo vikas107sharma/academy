f2f8f9fdfd351f87a2de238264b431cefd190778
vikas107sharma <vikas1141sharma@gmail.com>
2024-07-11 14:08:05 +0530

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    images: {
+      remotePatterns: [
+        { hostname: 'utfs.io'},
+        { hostname: 'img.clerk.com'}
+      ]
+    }
+  };
+  
+  export default nextConfig;
