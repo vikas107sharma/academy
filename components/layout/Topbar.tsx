4475e52e5dc3cbfb98d782d3eeced726aedec829
vikas107sharma <vikas1141sharma@gmail.com>
2024-07-20 15:32:58 +0530

    some important changes

--- components/layout/Topbar.tsx
+++ components/layout/Topbar.tsx
@@ -47,7 +47,7 @@ const Topbar = () => {
   return (
     <div className="flex justify-between items-center p-4">
       <Link href="/">
-        <Image src="/logo.png" height={100} width={200} alt="logo" />
+        <Image src="/logo.png" height={70} width={140} alt="logo" />
       </Link>
 
       <div className="max-md:hidden w-[400px] rounded-full flex">
